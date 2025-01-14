5bd02e4e457ceaad7c6ebd8af8fb9754e5f60f6b
Abraham Hamidi <abraham.hamidi01@gmail.com>
2025-01-14 16:31:51 -0600

    Remove onefile from build scripts

--- build.ps1
+++ build.ps1
@@ -16,16 +16,15 @@ python -m pip install -r requirements.txt
 
 # For Windows, ahrs/utils should be located in:
 #     .venv\Lib\site-packages\ahrs\utils
-$AHRS_UTILS_PATH = ".\.venv\Lib\site-packages\ahrs\utils"
+$AHRS_UTILS_PATH = "./.venv/Lib/site-packages/ahrs/utils"
 # Confirm that directory exists
 if (-not (Test-Path $AHRS_UTILS_PATH)) {
-    Write-Error "Could not find 'ahrs/utils' in .venv\Lib\site-packages. Make sure AHRS is installed."
+    Write-Error "Could not find 'ahrs/utils' in .venv/Lib/site-packages. Make sure AHRS is installed."
         exit 1
 }
 
 # Build with PyInstaller, adding the data folder
-python -m PyInstaller --add-data "$AHRS_UTILS_PATH;ahrs/utils" --onefile app.py
+python -m PyInstaller --add-data "$AHRS_UTILS_PATH;ahrs/utils" app.py
 
 # Create the tar.gz archive (requires tar in PATH)
 tar -czvf dist/dist.tar.gz dist/app
-
--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ python -m pip install -r requirements.txt
 # Dynamically find the Python version directory inside .venv/lib
 PYTHON_LIB_PATH=$(find .venv/lib -type d -name "python3.*" -print -quit)
 AHRS_UTILS_PATH="$PYTHON_LIB_PATH/site-packages/ahrs/utils"
-python -m PyInstaller --add-data "$AHRS_UTILS_PATH:ahrs/utils" --onefile app.py
+python -m PyInstaller --add-data "$AHRS_UTILS_PATH:ahrs/utils" app.py
 tar -czvf dist/dist.tar.gz dist/main
